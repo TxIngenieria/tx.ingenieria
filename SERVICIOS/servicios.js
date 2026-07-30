0ab2fe5dbf4677e6e11bf19c7f3ff583c596b978
TxIngenieria <txtructura.ingenieria@gmail.com>
2026-07-29 21:45:48 -0500

    Update servicios.js

--- SERVICIOS/servicios.js
+++ SERVICIOS/servicios.js
@@ -87,7 +87,7 @@ const servicios = {
   "distribuidor-aire": {
     titulo: "Fabricación de Distribuidor de aire (industrial)",
     descripcion: "Producción de distribuidores de aire de alto rendimiento para sistemas neumáticos industriales.",
-    imagenes: ["imageness/fabdistr.jpeg","imageness/fabdistr01.png"],
+    imagenes: ["imageness/fabdistr.jpeg","imageness/fabdistr001.jpeg"],
     detalles: [
       "Distribución uniforme",
       "Control de flujo",
